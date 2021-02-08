316469da72d73998e0e1816e0f62fcecbb72c087
yunsell <yunsell31@gmail.com>
2021-02-08 18:57:13 +0900

    v0.0.3

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,22 +1,29 @@
 from datetime import datetime
+
 from flask import Blueprint, render_template, request, url_for
 from werkzeug.utils import redirect
+
 from .. import db
-from ..models import Question
 from ..forms import QuestionForm, AnswerForm
+from ..models import Question
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
+
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
+
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html', question=question, form=form)
+
 
 @bp.route('/create/', methods=('GET', 'POST'))
 def create():
