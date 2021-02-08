497dea7487af369fb6fe0e976e89229e05f933dd
yunsell <yunsell31@gmail.com>
2021-02-08 17:53:22 +0900

    v0.0.2

---
+++ README.md
@@ -0,0 +1,8 @@
+#AICMS TEST
+
+##v0.0.2
+- 베이스화면 및 bootstrap 설정
+```
+##v0.0.1
+- flask, sqlite 작동
+```
--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+SECRET_KEY = "dev"
+
 BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
